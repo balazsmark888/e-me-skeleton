e89dc9489244f32751e5d8d5c70b3dd443478ccb
Balazs Mark <balazsmark888@users.noreply.github.com>
2020-02-06 12:03:06 +0200

    Add project files.

---
+++ e-me.mobile/e-me.mobile/MainPage.xaml.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace e_me.mobile
+{
+    // Learn more about making custom code visible in the Xamarin.Forms previewer
+    // by visiting https://aka.ms/xamarinforms-previewer
+    [DesignTimeVisible(false)]
+    public partial class MainPage : ContentPage
+    {
+        public int Counter { get; set; } = 0;
+
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        private void Button_OnClicked(object sender, EventArgs e)
+        {
+            Counter++;
+            ((Button)sender).Text = $"You clicked {Counter} times.";
+        }
+    }
+}
